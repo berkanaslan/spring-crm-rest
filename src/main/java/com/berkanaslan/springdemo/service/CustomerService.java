31ee9e8d172084a003d9a44d99dc97e27d9d41cd
Berkan Aslan <aslnberkan@gmail.com>
2021-04-04 16:45:24 +0300

    Initialized.

---
+++ src/main/java/com/berkanaslan/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.berkanaslan.springdemo.service;
+
+import java.util.List;
+
+import com.berkanaslan.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+
+}
