4eacd34adbbfcc1f4f0d6d21dbcf703397fcac6e
Berkan Aslan <aslnberkan@gmail.com>
2021-04-04 17:06:05 +0300

    getCustomer(s) added.

---
+++ src/main/java/com/berkanaslan/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,34 @@
+package com.berkanaslan.springdemo.rest;
+
+import com.berkanaslan.springdemo.entity.Customer;
+import com.berkanaslan.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Berkan Aslan
+ * 4.04.2021
+ */
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        return customerService.getCustomer(customerId);
+    }
+}
